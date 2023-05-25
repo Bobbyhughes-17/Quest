8aad3b539acca764957447ce51e78d798d551395
Bobby Hughes <bobbyhughes6316@gmail.com>
2023-05-25 13:13:28 -0400

    phase 3

--- Program.cs
+++ Program.cs
@@ -14,6 +14,11 @@ namespace Quest
             //   the text of the challenge
             //   a correct answer
             //   a number of awesome points to gain or lose depending on the success of the challenge
+
+            bool repeatQuest = true;
+
+            while (repeatQuest)
+            {
             Challenge twoPlusTwo = new Challenge("2 + 2?", 4, 10);
             Challenge theAnswer = new Challenge(
                 "What's the answer to life, the universe and everything?", 42, 25);
@@ -43,7 +48,10 @@ namespace Quest
             int maxAwesomeness = 100;
 
             // Make a new "Adventurer" object using the "Adventurer" class
-            Adventurer theAdventurer = new Adventurer("Jack");
+            Console.WriteLine("Enter adventurer name");
+            string userName = Console.ReadLine();
+
+            Adventurer theAdventurer = new Adventurer(userName);
 
             // A list of challenges for the Adventurer to complete
             // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
@@ -75,6 +83,18 @@ namespace Quest
             else
             {
                 Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+            }
+
+                Console.WriteLine("Do you want to repeat quest? (yes/no): ");
+                string repeat = Console.ReadLine();
+
+                if (repeat.ToLower() != "yes")
+                {
+                    repeatQuest = false;
+                }
+
+                Console.WriteLine();
+
             }
         }
     }
